5842fddf150d655a09eebdbed300a977d7357b13
Alejandro <79596635+copeta08@users.noreply.github.com>
2021-05-26 01:45:38 +0200

    Añadidos archivos adicionales de solucion

---
+++ Ejercicio08_UD06/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Ejercicio08_UD06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bSorteo = new System.Windows.Forms.Button();
+            this.bJugar = new System.Windows.Forms.Button();
+            this.bResultado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bSorteo
+            // 
+            this.bSorteo.Location = new System.Drawing.Point(62, 40);
+            this.bSorteo.Name = "bSorteo";
+            this.bSorteo.Size = new System.Drawing.Size(200, 39);
+            this.bSorteo.TabIndex = 0;
+            this.bSorteo.Text = "Realizar sorteo";
+            this.bSorteo.UseVisualStyleBackColor = true;
+            this.bSorteo.Click += new System.EventHandler(this.bSorteo_Click);
+            // 
+            // bJugar
+            // 
+            this.bJugar.Location = new System.Drawing.Point(62, 109);
+            this.bJugar.Name = "bJugar";
+            this.bJugar.Size = new System.Drawing.Size(200, 39);
+            this.bJugar.TabIndex = 1;
+            this.bJugar.Text = "Introducir números del jugador";
+            this.bJugar.UseVisualStyleBackColor = true;
+            this.bJugar.Click += new System.EventHandler(this.bJugar_Click);
+            // 
+            // bResultado
+            // 
+            this.bResultado.Location = new System.Drawing.Point(62, 180);
+            this.bResultado.Name = "bResultado";
+            this.bResultado.Size = new System.Drawing.Size(200, 39);
+            this.bResultado.TabIndex = 2;
+            this.bResultado.Text = "Resultado";
+            this.bResultado.UseVisualStyleBackColor = true;
+            this.bResultado.Click += new System.EventHandler(this.bResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 267);
+            this.Controls.Add(this.bResultado);
+            this.Controls.Add(this.bJugar);
+            this.Controls.Add(this.bSorteo);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 08";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bSorteo;
+        private System.Windows.Forms.Button bJugar;
+        private System.Windows.Forms.Button bResultado;
+    }
+}
+
---
+++ Ejercicio08_UD06/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using Microsoft.VisualBasic;
+
+namespace Ejercicio08_UD06
+{
+    public partial class Form1 : Form
+    {
+        const int kNum = 6;
+
+        ArrayList numSorteo = new ArrayList(kNum);
+        ArrayList numJugador = new ArrayList(kNum);
+
+        void realizarSorteo(ArrayList numerosSorteo)
+        {
+            int cont, num;
+            Random r = new Random();
+
+            cont = 0;
+            while (cont < kNum)
+            {
+                num = r.Next(1, 50);
+
+                if (numerosSorteo.Contains(num) == false)
+                {
+                    numerosSorteo.Add(num);
+                    cont++;
+                }
+            }
+        }
+
+        void introducirNum(ArrayList list)
+        {
+
+            int i = 0;
+
+            while (i < 6)
+            {
+                int num = int.Parse(Interaction.InputBox("Introduce un número"));
+
+                if (num > 0 && num < 50 && list.Contains(num) == false)
+                {
+                    list.Add(num);
+                    i++;
+                }
+                else
+                {
+                    MessageBox.Show("El número debe estar entre el 1 y el 49 y no debe estar repetido.");
+                }
+            }
+        }
+
+        string mostrarLista(ArrayList lis1)
+        {
+            string texto;
+
+            texto = "";
+            for (int i = 0; i < lis1.Count; i++)
+            {
+                texto = texto + lis1[i] + ", ";
+            }
+
+            texto = texto + "\n";
+
+            return texto;
+        }
+
+        int comprobarResultado(ArrayList listSorteo, ArrayList listJugador)
+        {
+            int cont = 0;
+
+            for (int i = 0; i < kNum; i++)
+            {
+                if ((int)listSorteo[i] == (int)listJugador[i])
+                {
+                    cont++;
+                }
+            }
+
+            return cont;
+        } //función que comprueba que los números coincidan línea por línea.
+
+        /*int comprobarRes(ArrayList lisSorteo, ArrayList lisJugador) 
+        {
+            int conta = 0;
+
+            for (int i = 0; i < kNum; i++)
+            {
+                for (int j = 0; j < kNum; j++)
+                {
+                    if ((int)lisJugador[i] == (int)lisSorteo[j])
+                    {
+                        conta++;
+                    }
+                }
+            }
+            return conta;
+        }*/   //función que comprueba que los número introducidos son iguales sin importar el orden
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bSorteo_Click(object sender, EventArgs e)
+        {
+            numSorteo.Clear();
+            realizarSorteo(numSorteo);
+
+            MessageBox.Show(mostrarLista(numSorteo));
+        }
+
+        private void bJugar_Click(object sender, EventArgs e)
+        {
+            numJugador.Clear();
+            introducirNum(numJugador);
+
+            MessageBox.Show(mostrarLista(numJugador));
+        }
+
+        private void bResultado_Click(object sender, EventArgs e)
+        {
+            int total = comprobarResultado(numSorteo, numJugador);
+
+            string resultado = "Coinciden " + total + " números. \n\n" + mostrarLista(numSorteo) + mostrarLista(numJugador);
+
+            MessageBox.Show(resultado);
+        }
+    }
+}
